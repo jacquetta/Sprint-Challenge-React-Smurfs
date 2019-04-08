f1a35f677920830154fb8c99ff795b039a3c8109
jacquetta <jacquettavb@hotmail.com>
2019-04-08 00:33:55 -0500

    updated styles on homepage

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import './smurf.css'
+
 const Smurf = props => {
   return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
+      <div className="Smurf col-sm-4 card">
+        <div className='card-body'>
+          <h3 className='card-title'>{props.name}</h3>
+          <strong>{props.height} tall</strong>
+          <p className='card-text'>{props.age} smurf years old</p>
+        </div>
+        </div>
   );
 };
 
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Smurf from './Smurf';
+import './smurf.css';
 
 class Smurfs extends Component {
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
-        <ul>
+        <ul className='row'>
           {this.props.smurfs.map(smurf => {
             return (
               <Smurf
